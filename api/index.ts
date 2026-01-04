b30d0d3bb9330ae987e5acd9fb1747d0e8ab66ba
sanjay jaiswal <sanjayjaiswal@sanjays-MacBook-Air.local>
2026-01-04 17:37:57 +0530

    s

--- api/index.ts
+++ api/index.ts
@@ -1,28 +1,21 @@
 import express from "express";
-import dotenv from "dotenv";
 import cors from "cors";
+import dotenv from "dotenv";
 
 import { getUserProfileAndRepos } from "./getUserInfo.js";
 import { analyzeGithubProfile } from "./gptControl.js";
 
-
+dotenv.config();
 
 const app = express();
 
-dotenv.config();
-
 app.use(cors({
   origin: "*",
-  credentials: true,
 }));
 
 app.use(express.json());
 
-
-
 app.get("/api/user/:username", getUserProfileAndRepos);
 app.get("/api/groq/:username", analyzeGithubProfile);
-
-const PORT = process.env.PORT || 5000;
 
 export default app;
